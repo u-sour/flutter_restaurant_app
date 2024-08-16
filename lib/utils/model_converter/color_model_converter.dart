3f581c4e76c1024adb6f33b92446814d504c2044
u-sour <yousour.dev@gmail.com>
2024-08-16 08:52:46 +0700

    add icon,date and color model converter

---
+++ lib/utils/model_converter/color_model_converter.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+
+class ColorModelConverter {
+  static Color convertColorForModel(Color color) => color;
+  static Color? convertColorOptionalForModel(Color? color) => color;
+}
