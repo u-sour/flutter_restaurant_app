a4b29b1331aa8918a354d8c6e53d28635ed9788a
u-sour <yousour.dev@gmail.com>
2024-12-21 10:31:19 +0700

    bug fix 21-12-24

--- lib/restaurant/widgets/invoice-template/invoice_template_header_widget.dart
+++ lib/restaurant/widgets/invoice-template/invoice_template_header_widget.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_printer/flutter_bluetooth_printer_library.dart';
-import 'package:provider/provider.dart';
-import '../../../providers/printer_provider.dart';
 import '../../../utils/constants.dart';
 import '../../models/invoice-template/header/header_list_schema_model.dart';
 import '../../models/invoice-template/header/header_schema_model.dart';
@@ -10,11 +8,13 @@ import '../../utils/sale/sale_utils.dart';
 import 'invoice_template_cached_img_widget.dart';
 
 class InvoiceTemplateHeaderWidget extends StatelessWidget {
+  final PaperSize paperSize;
   final String ipAddress;
   final HeaderSchemaModel headerSchema;
   final SaleInvoiceForPrintModel sale;
   const InvoiceTemplateHeaderWidget({
     super.key,
+    required this.paperSize,
     required this.ipAddress,
     required this.headerSchema,
     required this.sale,
@@ -23,79 +23,82 @@ class InvoiceTemplateHeaderWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final PrinterProvider readPrinterProvider = context.read<PrinterProvider>();
-    final PaperSize paperSize = readPrinterProvider.controller.paperSize;
     const Color baseColor = Colors.black;
     List<HeaderListSchemaModel> headers = headerSchema.list;
     return Column(
       children: [
         for (int i = 0; i < headers.length; i++)
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Type : Logo
-              if (headers[i].type == 'logo' && headers[i].imageUrl != null)
-                InvoiceTemplateCachedImgWidget(
-                  ipAddress: ipAddress,
-                  imgUrl: headers[i].imageUrl!,
-                  width: headers[i].valueStyle.width ?? 0.0,
-                  height: headers[i].valueStyle.width ?? 0.0,
-                  margin: EdgeInsetsDirectional.only(
-                      top: headers[i].wrapperStyle?.marginTop ?? 0.0),
-                  borderRadius: convertValueFromCSS(
-                      value: headers[i].valueStyle.borderRadius!),
-                ),
-              // Type: Header
-              if (headers[i].isVisible != false)
-                RichText(
-                  // Label
-                  text: TextSpan(
-                      text: headers[i].label,
-                      style: theme.textTheme.bodySmall!.copyWith(
-                          color: baseColor,
-                          fontSize: paperSize == PaperSize.mm80 &&
-                                  headers[i].labelStyle != null
-                              ? headers[i].labelStyle!.fontSize! +
-                                  AppStyleDefaultProperties.iefs
-                              : headers[i].labelStyle?.fontSize,
-                          fontWeight: headers[i].labelStyle?.fontWeight !=
-                                      null &&
-                                  headers[i].labelStyle?.fontWeight == 'bold'
-                              ? FontWeight.bold
-                              : null),
-                      children: [
-                        // Value
-                        WidgetSpan(
-                          child: Container(
-                            margin: EdgeInsets.only(
-                                left: headers[i].labelStyle?.marginRight ?? 0.0,
-                                top: headers[i].valueStyle.marginTop ?? 0.0),
-                            child: Text(
-                              headers[i].field == 'tableName' ||
-                                      headers[i].field == 'orderNum'
-                                  ? sale.toJson()[headers[i].field]
-                                  : headers[i].value ?? '',
-                              style: theme.textTheme.bodySmall!.copyWith(
-                                  color: baseColor,
-                                  fontSize: paperSize == PaperSize.mm80 &&
-                                          headers[i].valueStyle.fontSize != null
-                                      ? headers[i].valueStyle.fontSize! +
-                                          AppStyleDefaultProperties.iefs
-                                      : headers[i].valueStyle.fontSize,
-                                  fontWeight: headers[i]
-                                                  .valueStyle
-                                                  .fontWeight !=
-                                              null &&
-                                          headers[i].valueStyle.fontWeight ==
-                                              'bold'
-                                      ? FontWeight.bold
-                                      : null),
+          SizedBox(
+            width: double.maxFinite,
+            child: Wrap(
+              alignment: WrapAlignment.center,
+              children: [
+                // Type : Logo
+                if (headers[i].type == 'logo' && headers[i].imageUrl != null)
+                  InvoiceTemplateCachedImgWidget(
+                    ipAddress: ipAddress,
+                    imgUrl: headers[i].imageUrl!,
+                    width: headers[i].valueStyle.width ?? 0.0,
+                    height: headers[i].valueStyle.width ?? 0.0,
+                    margin: EdgeInsetsDirectional.only(
+                        top: headers[i].wrapperStyle?.marginTop ?? 0.0),
+                    borderRadius: convertValueFromCSS(
+                        value: headers[i].valueStyle.borderRadius!),
+                  ),
+                // Type: Header
+                if (headers[i].isVisible != false)
+                  RichText(
+                    // Label
+                    text: TextSpan(
+                        text: headers[i].label,
+                        style: theme.textTheme.bodySmall!.copyWith(
+                            color: baseColor,
+                            fontSize: paperSize == PaperSize.mm80 &&
+                                    headers[i].labelStyle != null
+                                ? headers[i].labelStyle!.fontSize! +
+                                    AppStyleDefaultProperties.iefs
+                                : headers[i].labelStyle?.fontSize,
+                            fontWeight: headers[i].labelStyle?.fontWeight !=
+                                        null &&
+                                    headers[i].labelStyle?.fontWeight == 'bold'
+                                ? FontWeight.bold
+                                : null),
+                        children: [
+                          // Value
+                          WidgetSpan(
+                            child: Container(
+                              margin: EdgeInsets.only(
+                                  left:
+                                      headers[i].labelStyle?.marginRight ?? 0.0,
+                                  top: headers[i].valueStyle.marginTop ?? 0.0),
+                              child: Text(
+                                headers[i].field == 'tableName' ||
+                                        headers[i].field == 'orderNum'
+                                    ? sale.toJson()[headers[i].field]
+                                    : headers[i].value ?? '',
+                                style: theme.textTheme.bodySmall!.copyWith(
+                                    color: baseColor,
+                                    fontSize: paperSize == PaperSize.mm80 &&
+                                            headers[i].valueStyle.fontSize !=
+                                                null
+                                        ? headers[i].valueStyle.fontSize! +
+                                            AppStyleDefaultProperties.iefs
+                                        : headers[i].valueStyle.fontSize,
+                                    fontWeight: headers[i]
+                                                    .valueStyle
+                                                    .fontWeight !=
+                                                null &&
+                                            headers[i].valueStyle.fontWeight ==
+                                                'bold'
+                                        ? FontWeight.bold
+                                        : null),
+                              ),
                             ),
                           ),
-                        ),
-                      ]),
-                ),
-            ],
+                        ]),
+                  ),
+              ],
+            ),
           ),
       ],
     );
--- lib/restaurant/widgets/invoice-to-kitchen-template/invoice_to_kitchen_header_widget.dart
+++ lib/restaurant/widgets/invoice-to-kitchen-template/invoice_to_kitchen_header_widget.dart
@@ -1,56 +1,59 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_printer/flutter_bluetooth_printer_library.dart';
-import 'package:provider/provider.dart';
-import '../../../providers/printer_provider.dart';
 import '../../../utils/convert_date_time.dart';
 
 class InvoiceToKitchenHeaderWidget extends StatelessWidget {
+  final PaperSize paperSize;
   final String tableLocation;
   final String orderNum;
   const InvoiceToKitchenHeaderWidget(
-      {super.key, required this.tableLocation, required this.orderNum});
+      {super.key,
+      required this.paperSize,
+      required this.tableLocation,
+      required this.orderNum});
   final prefixInvoiceHeader = 'screens.sale.invoice.info';
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final PrinterProvider readPrinterProvider = context.read<PrinterProvider>();
-    final PaperSize paperSize = readPrinterProvider.controller.paperSize;
     const Color baseColor = Colors.black;
     return Column(
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            RichText(
-              text: TextSpan(
-                  // text: '$prefixInvoiceHeader.date'.tr(),
-                  style: paperSize == PaperSize.mm80
-                      ? theme.textTheme.headlineMedium!.copyWith(
-                          color: baseColor, fontWeight: FontWeight.bold)
-                      : theme.textTheme.bodyLarge!.copyWith(
-                          color: baseColor, fontWeight: FontWeight.bold),
-                  children: [
-                    TextSpan(
-                        text: ConvertDateTime.formatTimeStampToString(
-                            DateTime.now(), false,
-                            formatStyle: 'dd/MM/yyyy hh:mm a'))
-                  ]),
-            ),
-            RichText(
-              text: TextSpan(
-                  // text: '$prefixInvoiceHeader.tableLocation'.tr(),
-                  style: paperSize == PaperSize.mm80
-                      ? theme.textTheme.headlineMedium!.copyWith(
-                          color: baseColor, fontWeight: FontWeight.bold)
-                      : theme.textTheme.bodyLarge!.copyWith(
-                          color: baseColor, fontWeight: FontWeight.bold),
-                  children: [
-                    TextSpan(text: tableLocation),
-                  ]),
-            )
-          ],
+        SizedBox(
+          width: double.maxFinite,
+          child: Wrap(
+            alignment: WrapAlignment.spaceBetween,
+            children: [
+              RichText(
+                text: TextSpan(
+                    // text: '$prefixInvoiceHeader.date'.tr(),
+                    style: paperSize == PaperSize.mm80
+                        ? theme.textTheme.headlineMedium!.copyWith(
+                            color: baseColor, fontWeight: FontWeight.bold)
+                        : theme.textTheme.bodyLarge!.copyWith(
+                            color: baseColor, fontWeight: FontWeight.bold),
+                    children: [
+                      TextSpan(
+                          text: ConvertDateTime.formatTimeStampToString(
+                              DateTime.now(), false,
+                              formatStyle: 'dd/MM/yyyy hh:mm a'))
+                    ]),
+              ),
+              RichText(
+                text: TextSpan(
+                    // text: '$prefixInvoiceHeader.tableLocation'.tr(),
+                    style: paperSize == PaperSize.mm80
+                        ? theme.textTheme.headlineMedium!.copyWith(
+                            color: baseColor, fontWeight: FontWeight.bold)
+                        : theme.textTheme.bodyLarge!.copyWith(
+                            color: baseColor, fontWeight: FontWeight.bold),
+                    children: [
+                      TextSpan(text: tableLocation),
+                    ]),
+              )
+            ],
+          ),
         ),
         if (orderNum.isNotEmpty)
           RichText(
